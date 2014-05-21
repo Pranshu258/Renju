7141d43991d6ad3d6c740cfaee7c6a07b31fb789
Pranshu Gupta <pranshug258@gmail.com>
2014-05-21 17:23:21 +0530

    here is the complete version of flappy bird.

--- game.py
+++ game.py
@@ -1,78 +1,90 @@
-#We have used the pygame library to make a game something like the flappy bird
-#bif is the background image file
-#mif is the moving image file
-bif = "back.jpg"
-mif = "mif.png"
-#importing the libraries
-import pygame, sys
+im1='bird.png'
+im2='back2.png'
+im3='back1.jpg'
+speed_front=200
+cyan=(0,255,255)
+green=(115,237,24)
+
+
+import pygame,sys
+import random
 from pygame.locals import *
 
+highname='rhythm das'
+highscore=0
+ax,x,y,set=1000,0,500,0
+t=0
+c=0
+count=0
+
+
+fread=open('high scores.txt','r')     #for high scores management
+
 pygame.init()
-#setting the screen to 640 x 360 resolutin
-screen = pygame.display.set_mode((640,360),0,32)
-#loading the background and moving images
-background = pygame.image.load(bif).convert()
-mouse_c = pygame.image.load(mif).convert_alpha()
-#initial position of the moving image
-x, y = 1, 1
-movex, movey = 0, 0
-#to measure time (we will use this to decrease the speed of the moving image)
-#because by default its speed is high
+screen=pygame.display.set_mode((1000,550),0,0)
+background=pygame.image.load(im2).convert_alpha()
+bird=pygame.image.load(im1).convert_alpha()
+back=pygame.image.load(im3).convert()
+screen.fill(cyan)
+
 clock = pygame.time.Clock()
-speed = 200
-#counting score
-score = 0
-#the game loop
+
 while True:
-    
+
     for event in pygame.event.get():
-        #to quit when the user clicks the close button
-        if event.type == QUIT:
+
+        if event.type==QUIT:
             pygame.quit()
             sys.exit()
-        #these statements let the player to move the figure in desired direction using the arrow keys
-        if event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                movex = -1
-            elif event.key == K_RIGHT:
-                movex = +1
-            elif event.key == K_UP:
-                movey = -1
-            elif event.key == K_DOWN:
-                movey = +1
-        #these statements leave the figure on its own again as soon as the player leaves the key 
-        if event.type == KEYUP:
-            if event.key == K_LEFT:
-                movex = 0
-            elif event.key == K_RIGHT:
-                movex = 0
-            elif event.key == K_UP:
-                movey = 0
-            elif event.key == K_DOWN:
-                movey = 0
-    #the following three lines decrease the moving speed of the figure
-    milli = clock.tick()
-    seconds = milli/1000.0
-    dm = seconds*speed
-    #to change the co-ordinates of the moving figure according to the arrow keys pressed by the user
-    #note that the co-ordinates refer to the upper left corner of the moving figure
-    x+=movex
-    y+=movey
-    #these statements display the figures according to the changing co-ordinates
-    screen.blit(background, (0,0))
-    screen.blit(mouse_c, (x, y))
-    #the automatic movement of the figure
-    x = x+dm
-    y = y+dm
-    #counting score when the figure ends up at right edge of window and emerges from the left
-    if x > 640:
-        x = 0
-        score = score + 100
-    #if the moving figure touches the upper or lower edges of the window the game ends  
-    if y <= 0 or y > 254:
-        print 'GAME OVER  \nYou scored : '
-        print score
+        if event.type==KEYDOWN:
+            if event.key==K_SPACE:
+                c=1
+    second=clock.tick()
+    milli=second/1000.0
+    df=milli*speed_front
+    if c==1 and x<10:
+        x+=1
+        set=0
+    else:
+        x=0
+        c=0
+
+    y+=x
+ 
+    y=y-set*set/1000
+    set+=df
+    #for acceleration
+    screen.blit(back,(0,0))
+    screen.blit(background,(0,0))
+    screen.blit(bird,(200,550-y))
+
+    #according to screen resolution polygon coordinates can be adjusted
+    
+    pygame.draw.polygon(screen,green,((ax,t-1000),(ax,t+150),(ax-40,t+150),(ax-40,t+180),(ax+100,t+180),(ax+100,t+150),(ax+60,t+150),(ax+60,t-1000)))
+    pygame.draw.polygon(screen,green,((ax,550+t+1000),(ax,t+550-150),(ax-40,t+550-150),(ax-40,t+550-180),(ax+100,t+550-180),(ax+100,t+550-150),(ax+60,t+550-150),(ax+60,t+550+1000)))
+
+    ax-=df
+
+    #conditions for checking collision, here the resolution of my bird was 55*35. Resolution function can be used
+
+    if y<0 or (ax-40<200 and ax + 100>200 and (550-y+35>t+550-180 or 550-y<t+ 180)):
+        print 'game over\n Your Score:\t',count
+        highname=fread.readlines()
+        highscore=int(highname[1])
         pygame.quit()
-        sys.exit()
+        if(highscore<count):
+            fwrite=open('e:/flappy/high scores.txt','w')
+            highname[0]=raw_input('enter your name: ')+'\n'
+            highname[1]=str(count)
+            fwrite.writelines(highname)
+        print '\n\nHIGHSCORE:\n%s%d'%(highname[0],int(highname[1]))
         
+        sys.exit()
+        #for random heights of the obstacles, here t is the height
+    if ax<0:
+        ax=1000
+        count+=1
+        t=160-(random.random()*1000)%320
+
     pygame.display.update()
+    
